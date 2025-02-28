c3bfd7ad32542d7653a5c39a659f718984ebba80
chera-mihiretu <cheramihiretucm@gmail.com>
2025-02-28 11:20:53 +0300

    Finishing bloc testing

---
+++ pill_reminder/test/medicine/domain/usecases/get_all_medicine_usecase_test.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pill_reminder/cores/error/failure.dart';
+import 'package:pill_reminder/features/medicine/domain/usecases/get_all_medicine_usecase.dart';
+
+import '../../../mock/mock_generator.mocks.dart';
+import '../../../test_data/medicine_test_data.dart';
+
+void main() {
+  late GetAllMedicineUsecase useCase;
+  late MockMedicineRepository mockMedicineRepository;
+
+  setUp(() {
+    mockMedicineRepository = MockMedicineRepository();
+    useCase = GetAllMedicineUsecase(mockMedicineRepository);
+  });
+
+  final testData = MedicineTestData.medicineEntity;
+
+  final testDataList = [
+    testData,
+  ];
+
+  const errorTest = "Test Error";
+
+  test('should get all medicines from the repository', () async {
+    when(mockMedicineRepository.getMedicines())
+        .thenAnswer((_) async => Right(testDataList));
+
+    final result = await useCase.execute();
+
+    expect(result, Right(testDataList));
+    verify(mockMedicineRepository.getMedicines());
+    verifyNoMoreInteractions(mockMedicineRepository);
+  });
+
+  test('should return failure when repository fails', () async {
+    when(mockMedicineRepository.getMedicines())
+        .thenAnswer((_) async => const Left(CacheFailure(message: errorTest)));
+
+    final result = await useCase.execute();
+
+    expect(result, const Left(CacheFailure(message: errorTest)));
+    verify(mockMedicineRepository.getMedicines());
+    verifyNoMoreInteractions(mockMedicineRepository);
+  });
+}
---
+++ pill_reminder/test/medicine/domain/usecases/get_madicine_usecase_test.dart
@@ -0,0 +1,46 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pill_reminder/cores/error/failure.dart';
+import 'package:pill_reminder/features/medicine/domain/usecases/get_madicine_usecase.dart';
+import 'package:pill_reminder/features/medicine/domain/entities/medicine_entity.dart';
+
+import '../../../mock/mock_generator.mocks.dart';
+import '../../../test_data/medicine_test_data.dart';
+
+void main() {
+  late GetMadicineUsecase useCase;
+  late MockMedicineRepository mockMedicineRepository;
+
+  setUp(() {
+    mockMedicineRepository = MockMedicineRepository();
+    useCase = GetMadicineUsecase(mockMedicineRepository);
+  });
+
+  final testData = MedicineTestData.medicineEntity;
+  final medicineId = testData.medicineId;
+  const errorTest = "Test Error";
+
+  test('should get medicine from the repository', () async {
+    when(mockMedicineRepository.getMedicine(medicineId))
+        .thenAnswer((_) async => Right(testData));
+
+    final result = await useCase.execute(medicineId);
+
+    expect(result, Right(testData));
+    verify(mockMedicineRepository.getMedicine(medicineId));
+    verifyNoMoreInteractions(mockMedicineRepository);
+  });
+
+  test('should return failure when getting medicine from the repository fails',
+      () async {
+    when(mockMedicineRepository.getMedicine(medicineId))
+        .thenAnswer((_) async => const Left(CacheFailure(message: errorTest)));
+
+    final result = await useCase.execute(medicineId);
+
+    expect(result, const Left(CacheFailure(message: errorTest)));
+    verify(mockMedicineRepository.getMedicine(medicineId));
+    verifyNoMoreInteractions(mockMedicineRepository);
+  });
+}
